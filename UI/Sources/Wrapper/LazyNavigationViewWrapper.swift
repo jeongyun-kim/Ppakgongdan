434d57510fd18cd202793347e99cb78baa31685d
jeongyun-kim <kkanyo04@naver.com>
2024-11-04 18:48:25 +0900

    design: LazyNavigationView 구성

--- UI/Sources/Wrapper/LazyNavigationViewWrapper.swift
+++ UI/Sources/Wrapper/LazyNavigationViewWrapper.swift
@@ -5,4 +5,21 @@
 //  Created by 김정윤 on 11/4/24.
 //
 
-import Foundation
+import SwiftUI
+
+public struct LazyNavigationViewWrapper<Content: View>: View { // 감싸줄 뷰 = Content
+    // 감싸줄 뷰 빌드
+    private let build: () -> Content
+    
+    public init(@ViewBuilder content: () -> Content) {
+        self.build = content
+    }
+    
+    var body: some View {
+        build()
+    }
+    
+    init(_ build: @autoclosure @escaping () -> Content) {
+        self.build = build
+    }
+}
