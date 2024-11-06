ac1c9cd71280c69a03a111c9917a72614cde34d7
jeongyun-kim <kkanyo04@naver.com>
2024-11-06 15:38:56 +0900

    refactor: 불필요 코드 제거

--- Network/Sources/NetworkServices/Auth/NetworkService + Auth.swift
+++ Network/Sources/NetworkServices/Auth/NetworkService + Auth.swift
@@ -17,24 +17,4 @@ extension NetworkService {
         let result = await authProvider.request(.refreshToken)
         return try decodeResults(result, modelType: RefreshTokenModel.self)
     }
-    
-    public func getRefreshToken(completionHandler: @escaping(Result<RefreshTokenModel, ErrorCodes>) -> Void) {
-        authProvider.request(.refreshToken) { result in
-            switch result {
-            case .success(let value):
-                print(value.statusCode)
-                do {
-                    let result = try JSONDecoder().decode(RefreshTokenModel.self, from: value.data)
-                    completionHandler(.success(result))
-                } catch {
-                    print("Error")
-                    let result = try? JSONDecoder().decode(ErrorModel.self, from: value.data)
-                    print("ㄹㅇㄹㅇ", result)
-                }
-            case .failure(let error):
-                print("getRefreshError", error)
-                completionHandler(.failure(.E06))
-            }
-        }
-    }
 }
