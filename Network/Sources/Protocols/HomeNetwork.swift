8f4e65de54b7223dc7a6d8cc949086d7a687f44f
jeongyun-kim <kkanyo04@naver.com>
2024-11-13 19:57:28 +0900

    feat: HomeNetworkProtocol 정의

---
+++ Network/Sources/Protocols/HomeNetwork.swift
@@ -0,0 +1,16 @@
+//
+//  HomeNetwork.swift
+//  NetworkKit
+//
+//  Created by 김정윤 on 11/13/24.
+//
+
+import Foundation
+import Moya
+
+protocol HomeNetwork {
+    var homeProvider: MoyaProvider<HomeRouter> { get }
+    func getMyWorkspaces() async throws -> [Workspace]
+    func createWorkspace(name: String, desc: String, image: Data) async throws -> Workspace
+    func deleteWorkspace(groupId: String) async throws
+}
