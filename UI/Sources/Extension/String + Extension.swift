a3c3636b52ceccf5127b492766a4faa0f8484b27
jeongyun-kim <kkanyo04@naver.com>
2024-11-25 18:02:10 +0900

    feat: 서버 날짜 채팅용 날짜로 변환하는 함수 추가

--- UI/Sources/Extension/String + Extension.swift
+++ UI/Sources/Extension/String + Extension.swift
@@ -13,4 +13,17 @@ extension String {
         let result = APIKey.baseURL.appending(path: "/v1\(self)")
         return result
     }
+    
+    public func toChattingDate() -> String {
+        let isoFormatter = ISO8601DateFormatter()
+        guard let date = isoFormatter.date(from: self) else {
+            return ""
+        }
+    
+        let timeFormatter = DateFormatter()
+        timeFormatter.locale = Locale(identifier: "ko_KR")
+        timeFormatter.dateFormat = "hh:mm a"
+        
+        return timeFormatter.string(from: date)
+    }
 }
