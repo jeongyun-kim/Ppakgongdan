d70fe31d650c2327b52af591a182e787e332e714
jeongyun-kim <kkanyo04@naver.com>
2024-11-28 23:03:55 +0900

    feat: 채팅 새로 받아올 때마다 스크롤 맨아래로 내리기 및 불필요 코드 제거

--- Feature-Home/Sources/Channel/Chatting/ChannelChattingReducer.swift
+++ Feature-Home/Sources/Channel/Chatting/ChannelChattingReducer.swift
@@ -14,22 +14,27 @@ import ComposableArchitecture
 struct ChannelChattingReducer {
     @ObservableState
     struct State: Equatable {
-        init(selectedChannel: Shared<StudyGroupChannel?>, workspaceId: String?) {
+        init(selectedChannel: Shared<StudyGroupChannel?>, chatList: Shared<[ChannelChatting]>, workspaceId: String?) {
             _selectedChannel = selectedChannel
+            _chatList = chatList
             self.workspaceId = workspaceId
         }
         
         @Shared var selectedChannel: StudyGroupChannel?
+        @Shared var chatList: [ChannelChatting]
         var workspaceId: String?
         var text: String = ""
         var isEnabled = false
+        var isFocused = false
     }
     
     enum Action: BindableAction {
         case binding(BindingAction<State>)
-        case getChannelChats // 현 채널의 채팅 목록 받아오기
         case sendMyChat // 채팅보내기
         case sendedChat // 내 채팅 보낸 이후
+        case connectSocket // 소켓 연결 등록
+        case disconnectSocket // 소켓 연결 해제
+        case appendChat(ChannelChatting) // 실시간으로 새로 받아온 채팅 반영
     }
     
     var body: some Reducer<State, Action> {
@@ -39,22 +44,31 @@ struct ChannelChattingReducer {
             case .binding:
                 return .none
                 
-            case .getChannelChats:
-                return .run { [id = state.workspaceId, channel = state.selectedChannel] send in
-                    guard let id, let channel else { return }
-                    do {
-                        let result = try await NetworkService.shared.getChannelChats(workspaceId: id, channelId: channel.channelId, after: channel.createdAt)
-                        print(result)
-                    } catch {
-                        print(error)
+            case .connectSocket:
+                return .run { [channel = state.selectedChannel] send in
+                    guard let channel else { return }
+                    let continuation = await withCheckedContinuation { continuation in
+                        SocketService.shared.estabilishConnection(channelId: channel.channelId) { data in
+                            continuation.resume(returning: data)
+                        }
                     }
+                    await send(.appendChat(continuation.toChannelChatting()))
                 }
+                
+            case .appendChat(let chat):
+                print(state.chatList)
+                state.chatList.append(chat)
+                return .none
+                
+            case .disconnectSocket:
+                SocketService.shared.disconnectSocket()
+                return .none
                 
             case .sendMyChat:
                 return .run { [id = state.workspaceId, channel = state.selectedChannel, content = state.text] send in
                     guard let id, let channel else { return }
                     do {
-                        let result = try await NetworkService.shared.postMyChat(workspaceId: id, channelId: channel.channelId, content: content, files: [])
+                        let _ = try await NetworkService.shared.postMyChat(workspaceId: id, channelId: channel.channelId, content: content, files: [])
                         await send(.sendedChat)
                     } catch {
                         print(error)
--- Feature-Home/Sources/Channel/Chatting/ChannelChattingView.swift
+++ Feature-Home/Sources/Channel/Chatting/ChannelChattingView.swift
@@ -7,30 +7,55 @@
 
 import SwiftUI
 import UI
+import NetworkKit
+import Utils
 import ComposableArchitecture
 
+
 struct ChannelChattingView: View {
+    @FocusState private var isFocused: Bool
     @Bindable var store: StoreOf<ChannelChattingReducer>
 
     var body: some View {
-        VStack {
-            List(0..<10, rowContent: { i in
-                Text("\(i)")
-            })
-            .asPlainList()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .scrollDismissesKeyboard(.immediately)
-            
-            ChattingInputView($store.text) {
-                store.send(.sendMyChat)
+        ScrollViewReader { proxy in
+            VStack {
+                List(store.chatList, id: \.chatId, rowContent: { item in
+                    chattingRowView(item)
+                        .listRowSeparator(.hidden)
+                })
+                .asPlainList()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .scrollDismissesKeyboard(.immediately)
+                
+                ChattingInputView($store.text, isFocused: $isFocused) {
+                    store.send(.sendMyChat)
+                }
+            }
+            .onChange(of: store.chatList) { oldValue, newValue in
+                guard oldValue != newValue else { return }
+                guard let last = newValue.last else { return }
+                proxy.scrollTo(last.chatId)
             }
         }
         .navigationTitle("#\(store.selectedChannel?.name ?? "채널")")
         .toolbarRole(.editor)
         .toolbar(.hidden, for: .tabBar)
         .onAppear {
-            store.send(.getChannelChats)
+            store.send(.connectSocket)
+        }
+        .onDisappear {
+            store.send(.disconnectSocket)
         }
     }
 }
 
+extension ChannelChattingView {
+    // MARK: ChattingRowView
+    private func chattingRowView(_ item: ChannelChatting) -> some View {
+        if item.user.userId == UserDefaultsManager.shared.userId {
+            return MyChattingView(name: item.user.nickname, message: item.content, images: item.files, date: item.createdAt)
+        } else {
+            return FriendChattingView(name: item.user.nickname, message: item.content, images: item.files, date: item.createdAt)
+        }
+    }
+}
