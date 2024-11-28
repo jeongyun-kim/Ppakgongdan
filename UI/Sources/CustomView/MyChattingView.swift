85aec022ef9a6cc1988c268075c0f8ca44209af1
jeongyun-kim <kkanyo04@naver.com>
2024-11-28 17:29:46 +0900

    design: MyChattingView 구성

---
+++ UI/Sources/CustomView/MyChattingView.swift
@@ -0,0 +1,48 @@
+//
+//  MyChattingView.swift
+//  UI
+//
+//  Created by 김정윤 on 11/28/24.
+//
+
+import SwiftUI
+
+public struct MyChattingView: View {
+    public init(name: String, message: String, images: [String], date: String) {
+        self.name = name
+        self.message = message
+        self.images = images
+        self.date = date
+    }
+    
+    private let name: String
+    private let message: String
+    private let images: [String]
+    private let date: String
+    private let width: CGFloat = 244
+    
+    public var body: some View {
+        ZStack(alignment: .topLeading) {
+            HStack(alignment: .top, spacing: 8) {
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    Text(date.toChattingDate())
+                        .font(.caption2)
+                        .foregroundStyle(Resources.Colors.textSecondary)
+                    
+                    VStack(alignment: .leading) {
+                        ChatView(message: message)
+                        
+                        if !images.isEmpty {
+                            Resources.Images.defaultGroupImage
+                                .resizable()
+                                .frame(height: 162)
+                                .frame(maxWidth: width)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
