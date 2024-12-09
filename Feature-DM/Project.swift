c1423045adfdeb4ab38a6b3d5b34e3e73dbdbcb9
jeongyun-kim <kkanyo04@naver.com>
2024-12-09 22:20:37 +0900

    chore: Feature-DM 추가

--- Feature-DM/Project.swift
+++ Feature-DM/Project.swift
@@ -2,27 +2,24 @@
 //  Project.swift
 //  Config
 //
-//  Created by 김정윤 on 10/29/24.
+//  Created by 김정윤 on 12/9/24.
 //
 
 import Foundation
 import ProjectDescription
 
 let project = Project(
-    name: "Feature", // 프로젝트 명
+    name: "Feature-DM", // 프로젝트 명
     targets: [ // 프로젝트의 타겟
         .target(
-            name: "Feature",
+            name: "Feature-DM",
             destinations: [.iPhone], // 지원 기기 설정
             product: .framework,
-            bundleId: "com.jeongyun.feature",
+            bundleId: "com.jeongyun.feature.dm",
             deploymentTargets: .iOS("17.0"), // 지원 최소 버전 설정,
-            infoPlist: .extendingDefault(with: [:]),
             sources: ["Sources/**"],
             dependencies: [
-                .project(target: "Feature-Login", path: "../Feature-Login"),
-                .project(target: "Feature-Home", path: "../Feature-Home"),
-                .project(target: "Feature-DM", path: "../Feature-DM")
+                .project(target: "NetworkKit", path: "../Network")
             ]
         )
     ]
